efc8032c0891d40108a540c8aa098bda9ae2c9bb
pyropetrick <pavel.chernenko97@gmail.com>
2022-11-28 23:26:13 +0300

    feat(Auth): add wrapper for form inputs

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import { Button } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { ErrorMessage } from "../components";
 import { auth, login } from "../http/userApi";
@@ -30,36 +30,40 @@ export const Login = () => {
   mt-5 input-group"
       onSubmit={handleSubmit(submitForm)}
     >
-      <h2 className="text-black text-center">Login</h2>
-      <div className="d-flex gap-2 flex-column mb-3">
-        <label htmlFor="username" className="form-label">
-          Username
-        </label>
-        <input
-          type="text"
-          id="username"
-          {...register("username", {
-            required: "Username is required",
-            minLength: { value: 2, message: "Minimum characters 2" },
-          })}
-        />
-        {errors.username && <ErrorMessage message={errors.username.message} />}
-      </div>
-      <div className="d-flex gap-2 flex-column mb-3">
-        <label htmlFor="password" className="form-label">
-          Password
-        </label>
-        <input
-          type="password"
-          id="password"
-          {...register("password", {
-            required: "Password is required",
-            minLength: { value: 1, message: "Minimum characters 1" },
-          })}
-        />
-        {errors.password && <ErrorMessage message={errors.password.message} />}
-      </div>
-      <Button type="submit">Sign in</Button>
+      <Card className="p-3 rounded">
+        <h2 className="text-black text-center">Login</h2>
+        <div className="d-flex gap-2 flex-column mb-3">
+          <label htmlFor="username" className="form-label">
+            Username
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="username"
+            {...register("username", {
+              required: "Username is required",
+              minLength: { value: 2, message: "Minimum characters 2" },
+            })}
+          />
+          {errors.username && <ErrorMessage message={errors.username.message} />}
+        </div>
+        <div className="d-flex gap-2 flex-column mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            className="form-control"
+            type="password"
+            id="password"
+            {...register("password", {
+              required: "Password is required",
+              minLength: { value: 1, message: "Minimum characters 1" },
+            })}
+          />
+          {errors.password && <ErrorMessage message={errors.password.message} />}
+        </div>
+        <Button type="submit">Sign in</Button>
+      </Card>
     </form>
   );
 };
--- src/pages/Registration.tsx
+++ src/pages/Registration.tsx
@@ -1,4 +1,4 @@
-import { Button } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { ErrorMessage } from "../components/ErrorMessage";
 import { auth, registration } from "../http/userApi";
@@ -30,53 +30,55 @@ export const Registration = () => {
   mt-5 input-group"
       onSubmit={handleSubmit(submitForm)}
     >
-      <h2 className="text-black text-center">Registration</h2>
-      <div className="d-flex gap-2 flex-column mb-3">
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          id="username"
-          className="form-control"
-          {...register("username", {
-            required: "Username is required",
-            minLength: { value: 2, message: "Minimum characters 2" },
-          })}
-        />
-        {errors.username && <ErrorMessage message={errors.username.message} />}
-      </div>
-      <div className="d-flex gap-2 flex-column mb-3">
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          className="form-control"
-          {...register("password", {
-            required: "Password is required",
-            minLength: { value: 1, message: "Minimum characters 1" },
-          })}
-        />
-        {errors.password && <ErrorMessage message={errors.password.message} />}
-      </div>
-      <div className="d-flex gap-2 flex-column mb-3">
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          id="email"
-          className="form-control"
-          {...register("email", { required: true })}
-        />
-      </div>
-      <div className="d-flex gap-2 flex-column mb-3">
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" className="form-control" {...register("name")} />
-      </div>
-      <div className="d-flex gap-2 flex-column mb-3">
-        <label htmlFor="lastname">Last Name</label>
-        <input type="text" id="lastname" className="form-control" {...register("lastname")} />
-      </div>
-      <Button type="submit" variant="primary" className="rounded">
-        Sign up
-      </Button>
+      <Card className="p-3 rounded">
+        <h2 className="text-black text-center">Registration</h2>
+        <div className="d-flex gap-2 flex-column mb-3">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            className="form-control"
+            {...register("username", {
+              required: "Username is required",
+              minLength: { value: 2, message: "Minimum characters 2" },
+            })}
+          />
+          {errors.username && <ErrorMessage message={errors.username.message} />}
+        </div>
+        <div className="d-flex gap-2 flex-column mb-3">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            className="form-control"
+            {...register("password", {
+              required: "Password is required",
+              minLength: { value: 1, message: "Minimum characters 1" },
+            })}
+          />
+          {errors.password && <ErrorMessage message={errors.password.message} />}
+        </div>
+        <div className="d-flex gap-2 flex-column mb-3">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            className="form-control"
+            {...register("email", { required: true })}
+          />
+        </div>
+        <div className="d-flex gap-2 flex-column mb-3">
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" className="form-control" {...register("name")} />
+        </div>
+        <div className="d-flex gap-2 flex-column mb-3">
+          <label htmlFor="lastname">Last Name</label>
+          <input type="text" id="lastname" className="form-control" {...register("lastname")} />
+        </div>
+        <Button type="submit" variant="primary" className="rounded">
+          Sign up
+        </Button>
+      </Card>
     </form>
   );
 };
